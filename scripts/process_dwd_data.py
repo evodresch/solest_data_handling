b944379f5f808bce35b15378dcb2fb0e8a76615e
Evandro Dresch <evandro@live.de>
2024-06-07 14:15:21 +0200

    finished working on preprocessing scripts for temperature data

--- scripts/process_dwd_data.py
+++ scripts/process_dwd_data.py
@@ -27,38 +27,57 @@ def get_unprocessed_files(raw_data_path, processed_path):
     return unprocessed_files
 
 
-def parse_ascii_grid(file_path):
+def parse_ascii_grid(file_path, data_type):
     """
     Parse the ASCII grid file to separate the header and data sections.
 
     Args:
     file_path (str): The full path to the ASCII file.
+    data_type: The type of dwd data. Irradiation and temperature have different formats and need to be
+               handled differently
 
     Returns:
     tuple: A dictionary containing header key-value pairs and a list of data rows.
     """
     header = {}
     data = []
-    print("test")
-    with open(file_path, 'r') as file:
-        line = file.readline()
-        while line.strip() and not line.startswith("[ASCII-Raster-Format]"):
-            if '=' in line:
-                key, value = line.strip().split('=', 1)
-                header[key] = value
+
+    if data_type == 'radiation_global':
+        with open(file_path, 'r') as file:
             line = file.readline()
+            while line.strip() and not line.startswith("[ASCII-Raster-Format]"):
+                if '=' in line:
+                    key, value = line.strip().split('=', 1)
+                    header[key] = value
+                line = file.readline()
 
-        for line in file:
-            n_cells = len(line.split(" "))
+            for line in file:
+                n_cells = len(line.split(" "))
 
-            # ascii raster info
-            if n_cells == 2:
-                print(line)
-                key, value = line.strip().split(' ', maxsplit=1)
-                header[key] = value
+                # ascii raster info
+                if n_cells == 2:
+                    print(line)
+                    key, value = line.strip().split(' ', maxsplit=1)
+                    header[key] = value
 
-            else:
-                data.append(list(map(float, line.split(" "))))
+                else:
+                    data.append(list(map(float, line.split(" "))))
+
+    elif data_type == 'ambient_temperature':
+        with open(file_path, 'r') as file:
+            for line in file:
+                n_cells = len(line.split(" "))
+
+                # ascii raster info
+                if n_cells == 2:
+                    key, value = line.strip().split(' ', maxsplit=1)
+                    header[key] = value
+
+                elif n_cells > 2:
+                    line = line.strip()
+                    line = [el for el in line.split(" ") if el != '']
+                    line = [float(el) / 10 if el != '-999' else -999 for el in line]
+                    data.append(line)
     return header, data
 
 
@@ -106,7 +125,7 @@ def main():
         filepath_raw = os.path.join(save_path, f"{file_name}.asc")
         filepath_processed = os.path.join(processed_path, f"{file_name}.tif")
         logging.info(f"Processing file: {filepath_raw}")
-        header, data = parse_ascii_grid(filepath_raw)
+        header, data = parse_ascii_grid(filepath_raw, 'radiation_global')
         create_geotiff_from_data(header, data, filepath_processed)
         logging.info(f"Processed and saved to: {filepath_processed}")
 
@@ -118,7 +137,7 @@ def main():
         filepath_raw = os.path.join(save_path, f"{file_name}.asc")
         filepath_processed = os.path.join(processed_path, f"{file_name}.tif")
         logging.info(f"Processing file: {filepath_raw}")
-        header, data = parse_ascii_grid(filepath_raw)
+        header, data = parse_ascii_grid(filepath_raw, 'ambient_temperature')
         create_geotiff_from_data(header, data, filepath_processed)
         logging.info(f"Processed and saved to: {filepath_processed}")
 
